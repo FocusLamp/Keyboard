062f89828cb5330e7ad4dd1eed2000d3a02efa1d
Focus Lamp <focuslamp10@gmail.com>
2026-08-26 23:28:35 +0300

    removed license and updated setup script

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ sudo mkdir -p /etc/kanata/
 sudo cp colemak-dh-twilight.kbd /etc/kanata/colemak-dh-twilight.kbd
 sudo cp colemak-dh.kbd /etc/kanata/colemak-dh.kbd
 
-# sudo cp ./linux-binaries/kanata_linux_x64 /etc/kanata/
+sudo cp ./linux-binaries/kanata_linux_x64 /etc/kanata/
 
 sudo chmod 544 /etc/kanata/colemak-dh-twilight.kbd
 sudo chmod 544 /etc/kanata/colemak-dh.kbd
